9f4cfac5464df9df1083855dccd5ecb82602197e
YudaevAA <a.yudaev@ptvr.ru>
2018-09-26 19:50:11 +0300

    Реализованы функции поиска бара с минимальным и максимальным количеством мест

--- bars.py
+++ bars.py
@@ -1,21 +1,41 @@
 import json
+import math
+from functools import reduce
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf-8') as file:
+        parsed_json = json.loads(file.read())
+    return parsed_json
 
 
 def get_biggest_bar(data):
-    pass
+    return reduce(lambda x, y: x if max([x['properties']['Attributes']['SeatsCount'],
+                                         y['properties']['Attributes']['SeatsCount']]) \
+                                    == x['properties']['Attributes']['SeatsCount'] else y, data)
 
 
 def get_smallest_bar(data):
-    pass
+    return reduce(lambda x, y: x if min([x['properties']['Attributes']['SeatsCount'],
+                                         y['properties']['Attributes']['SeatsCount']]) \
+                                    == x['properties']['Attributes'][
+                                        'SeatsCount'] else y, data)
 
 
 def get_closest_bar(data, longitude, latitude):
+    """distance  = math.sqrt((x2-x1)**2+(y2-y1)**2)"""
+
     pass
+    # "geometry" "coordinates" [37.5808294018246, 55.759148105306608]
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('bars.json')
+    # print(data)
+    biggest_bar = get_biggest_bar(data['features'])
+    print(biggest_bar)
+    smallest_bar = get_smallest_bar(data['features'])
+    print(smallest_bar)
+    # longitude = input('Please input longitude: ')
+    # latitude = input('Please input latitude: ')
+    # get_closest_bar(data, longitude, latitude)
