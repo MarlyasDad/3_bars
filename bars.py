76d854c3127ddd74a339ad9bb841b463d7dd072c
MarlyasDad <yudaev.alex@gmail.com>
2018-09-27 14:28:29 +0300

    Добавлена функция вычисления наименьшего расстояния. Финальный коммит.

--- bars.py
+++ bars.py
@@ -11,31 +11,43 @@ def load_data(filepath):
 
 def get_biggest_bar(data):
     return reduce(lambda x, y: x if max([x['properties']['Attributes']['SeatsCount'],
-                                         y['properties']['Attributes']['SeatsCount']]) \
-                                    == x['properties']['Attributes']['SeatsCount'] else y, data)
+                                         y['properties']['Attributes']['SeatsCount']])
+                  == x['properties']['Attributes']['SeatsCount'] else y, data)
 
 
 def get_smallest_bar(data):
     return reduce(lambda x, y: x if min([x['properties']['Attributes']['SeatsCount'],
-                                         y['properties']['Attributes']['SeatsCount']]) \
-                                    == x['properties']['Attributes'][
-                                        'SeatsCount'] else y, data)
+                                         y['properties']['Attributes']['SeatsCount']])
+                  == x['properties']['Attributes']['SeatsCount'] else y, data)
 
 
 def get_closest_bar(data, longitude, latitude):
-    """distance  = math.sqrt((x2-x1)**2+(y2-y1)**2)"""
+    return reduce(lambda x, y: x if min([calc_distance(x['geometry']['coordinates'][1],
+                                                       x['geometry']['coordinates'][0],
+                                                       longitude,
+                                                       latitude),
+                                         calc_distance(y['geometry']['coordinates'][1],
+                                                       y['geometry']['coordinates'][0],
+                                                       longitude,
+                                                       latitude)])
+                  == calc_distance(x['geometry']['coordinates'][1],
+                                   x['geometry']['coordinates'][0],
+                                   longitude,
+                                   latitude) else y, data)
 
-    pass
-    # "geometry" "coordinates" [37.5808294018246, 55.759148105306608]
+def calc_distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
 if __name__ == '__main__':
     data = load_data('bars.json')
-    # print(data)
     biggest_bar = get_biggest_bar(data['features'])
     print(biggest_bar)
     smallest_bar = get_smallest_bar(data['features'])
     print(smallest_bar)
-    # longitude = input('Please input longitude: ')
-    # latitude = input('Please input latitude: ')
-    # get_closest_bar(data, longitude, latitude)
+    longitude = input('Please input longitude: ')
+    # longitude = 55.820875
+    latitude = input('Please input latitude: ')
+    # latitude = 37.604430
+    closest_bar = get_closest_bar(data['features'], longitude, latitude)
+    print(closest_bar)
